9f364c57356a62c52ae7461c687b8ed121ad0a73
yassir-69 <yassir.boualiqqq@gmail.com>
2023-09-04 19:04:10 +0100

    finch

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <elf.h>
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
