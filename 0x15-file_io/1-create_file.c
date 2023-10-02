00c2936f671610503e6d309273ee51583cbe3c1a
yassir-69 <yassir.boualiqqq@gmail.com>
2023-10-02 07:43:40 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,44 @@
 #include "main.h"
 
 /**
+ * length_of_a_string - returns the length of a string or
+ * Provides the size of a string
+ * @string: The string for which the length is measured.
+ * Return: The integer value representing the length of the string.
+ */
+int length_of_a_string(char *string)
+{
+int o = 0;
+if (!string)
+return (0);
+
+while (*string++)
+o++;
+return (o);
+}
+
+/**
  * create_file - Generates or Creates The File.
  * @filename: A pointer referencing the filename to be generated.
  * @text_content: A pointer to a string intended for writing into the file.
  *
- * Return: In case of failure, the function returns -1.
- * Alternatively, it returns 1.
+ * Return: Returns 1 when successful.
+ * Returns -1 when failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int length, zwri, hp = 0;
+int fd;
+ssize_t byte = 0, length = length_of_a_string(text_content);
 
-if (filename == NULL)
-	return (-1);
+if (!filename)
+return (-1);
 
-if (text_content != NULL)
-{
-	for (length = 0; text_content[length];)
-		length++;
-}
-	hp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	zwri = write(hp, text_content, length);
-if (hp == -1 || zwri == -1)
-	return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
 
-close(hp);
-return (1);
+if (length)
+byte = write(fd, text_content, length);
+close(fd);
+return (byte == length ? 1 : -1);
 }
